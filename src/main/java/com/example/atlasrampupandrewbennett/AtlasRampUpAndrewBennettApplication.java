0e005f356a4519aa4f2e4cccfb30f357641d41b6
Andrew Bennett <88630714+ACBennett7@users.noreply.github.com>
2022-09-06 21:24:58 -0400

    Remove commented code, load new version of Gradle, run spotlessApply

--- src/main/java/com/example/atlasrampupandrewbennett/AtlasRampUpAndrewBennettApplication.java
+++ src/main/java/com/example/atlasrampupandrewbennett/AtlasRampUpAndrewBennettApplication.java
@@ -1,6 +1,5 @@
 package com.example.atlasrampupandrewbennett;
 
-//import com.bettercloud.logging.constants.MDCKeys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,8 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class AtlasRampUpAndrewBennettApplication {
 
   public static void main(String[] args) {
-//    System.setProperty(MDCKeys.MICROSERVICE_NAME, "atlas-ramp-up-andrew-bennett");
-//    System.setProperty(MDCKeys.PILLAR, "ingestion");
     SpringApplication.run(AtlasRampUpAndrewBennettApplication.class, args);
   }
 }
